6d5cd17d4aa86cbb4378c82c7ca280721a1a20a4
vitalypaprotsky <vetalpaprotsky@gmail.com>
2017-01-24 19:21:17 +0200

    output info about order

--- app/controllers/bubbies/orders_controller.rb
+++ app/controllers/bubbies/orders_controller.rb
@@ -1,7 +1,9 @@
 module Bubbies
   class OrdersController < Spree::BaseController
+    include Spree::Core::ControllerHelpers::Order
 
     def new
+      render 'bubbies/orders/new', locals: { order: current_order }
     end
   end
 end
